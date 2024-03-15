37bf1f5af26d7a086983c806b393a3a3b9aca008
Oğuz Güngör <m.oguzgungor60.57@gmail.com>
2024-03-15 20:05:46 +0300

    home page and main layout have been added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
