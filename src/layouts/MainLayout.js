bb1f763cd2c44ce367d93626ea0839dfd32171f0
Oğuz Güngör <m.oguzgungor60.57@gmail.com>
2024-03-15 20:26:53 +0300

    main layout has been updated

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,10 +1,19 @@
+import { Layout } from "antd";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
 import { Outlet } from "react-router-dom";
+import "./MainLayout.scss";
 
 export default function MainLayout() {
   return (
-    <div>
-      layout container
-      <Outlet />
-    </div>
+    <Layout className="main-layout" style={{ width: "100vw", height: "100vh" }}>
+      <Header className="header">header</Header>
+      <Layout>
+        <Content className="content">
+          <Outlet />
+        </Content>
+      </Layout>
+      <Footer className="footer">footer</Footer>
+    </Layout>
   );
 }
